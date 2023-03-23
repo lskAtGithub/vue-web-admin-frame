2c9be28b5adb91804522cbe8cf546c1b31699d4b
liushengkai <949919743@qq.com>
2023-03-23 10:02:27 +0800

    引入 ElementPlus

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,12 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+
 import 'normalize.css/normalize.css'
 import './assets/main.css'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+app.use(ElementPlus)
+app.mount('#app')

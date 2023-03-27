08270597669b2092532d42333aa2a5d8ce9c690a
liushengkai <949919743@qq.com>
2023-03-27 19:18:47 +0800

    登录相关

--- src/api/user.ts
+++ src/api/user.ts
@@ -1,5 +1,6 @@
 import requert from '@/utils/request/index'
 
+import type { ILoginParam } from '@/Iparams/user'
 /**
  * @param 
  */
@@ -7,5 +8,26 @@ export const getMenu = () => {
   return requert({
     url: '/getMenu',
     method: 'get',
+  })
+}
+
+/**
+ * @param 
+ */
+export const getUserInfo = () => {
+  return requert({
+    url: '/userInfo',
+    method: 'get',
+  })
+}
+
+/**
+ * @param 
+ */
+export const getToken = (data: ILoginParam) => {
+  return requert({
+    url: '/login',
+    method: 'post',
+    data
   })
 }

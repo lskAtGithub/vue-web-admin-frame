bf591b75f90ed5cd0e5c10b85e34a5c4e11a1e91
liushengkai <949919743@qq.com>
2023-03-27 19:20:27 +0800

    home

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -1,31 +1,18 @@
 <script setup lang="ts">
 import { getMenu } from '@/api/user'
 import { onMounted } from 'vue'
-import { useStore } from '@/store/index'
 
-const counterStore  = useStore()
 
 onMounted(() => {
-  counterStore.couter++;
-  console.log(counterStore.couter)
-  
   getMenu().then(res => {
     console.log(res);
   })
 })
-function handleChange() {
-  counterStore.name = 'This is new counterStore name'
-}
-function handleReset() {
-  counterStore.$reset()
-}
+
 </script>
 
 <template>
   <div>
-    <p>Store 的name是 {{ counterStore.name }},</p>
-    <p>Store 的couter是 {{ counterStore.couter }}</p>
-    <button @click="handleChange">改变name</button>
-    <button @click="handleReset">reset counterStore</button>
+    navxxxx
   </div>
 </template>

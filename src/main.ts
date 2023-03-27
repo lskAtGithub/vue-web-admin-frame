1d69fe6162d54afa0c3b870412036b29e658c1dd
liushengkai <949919743@qq.com>
2023-03-27 19:18:20 +0800

    引入 permission

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,16 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router/index'
+import piniaStore from '@/store/index'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 import '../mock'
-import { createPinia } from 'pinia'
 import 'normalize.css/normalize.css'
 import './permission'
 
 const app = createApp(App)
-app.use(createPinia())
-app.use(ElementPlus)
+app.use(piniaStore)
 app.use(router)
+app.use(ElementPlus)
 app.mount('#app')
 

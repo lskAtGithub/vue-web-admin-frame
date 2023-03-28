c297438adcbd8752939725a9cd1336fcd8f0c0ca
liushengkai <949919743@qq.com>
2023-03-28 17:42:43 +0800

    引入并注册Icon

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import App from './App.vue'
 import router from './router/index'
 import piniaStore from '@/store/index'
 import ElementPlus from 'element-plus'
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 import 'element-plus/dist/index.css'
 import '../mock'
 import 'normalize.css/normalize.css'
@@ -15,3 +16,6 @@ app.use(router)
 app.use(ElementPlus)
 app.mount('#app')
 
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+  app.component(key, component)
+}

7d6436f37196f9d6437b585ad64cca58a1af82ca
liushengkai <949919743@qq.com>
2023-03-28 18:15:01 +0800

    element-plus 引入国际化

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import App from './App.vue'
 import router from './router/index'
 import piniaStore from '@/store/index'
 import ElementPlus from 'element-plus'
+import zhCn from 'element-plus/dist/locale/zh-cn.mjs'
 import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 import 'element-plus/dist/index.css'
 import '../mock'
@@ -13,7 +14,9 @@ import '@/styles/index.scss'
 const app = createApp(App)
 app.use(piniaStore)
 app.use(router)
-app.use(ElementPlus)
+app.use(ElementPlus, {
+  locale: zhCn,
+})
 app.mount('#app')
 
 for (const [key, component] of Object.entries(ElementPlusIconsVue)) {

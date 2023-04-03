525b526ae6d5c8e68c0642fdb70f5d1dceb3c56c
liushengkai <949919743@qq.com>
2023-04-03 18:12:23 +0800

    格式化工具类

---
+++ src/utils/FormatUtils.ts
@@ -0,0 +1,12 @@
+export default class FormatUtils {
+  /**
+   * @param {Array} arr
+   * @returns {Array}
+   */
+  static flattenArray(arr: Array<any>) {
+    while (arr.some(i => Array.isArray(i))) {
+      arr = [].concat(...arr)
+    }
+    return arr
+  }
+}

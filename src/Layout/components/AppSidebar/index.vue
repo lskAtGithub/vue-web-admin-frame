035a2bcb9b305e99a6588d6a4d472f5469aea7cd
liushengkai <949919743@qq.com>
2023-04-03 16:05:02 +0800

    Layout adjust

--- src/Layout/components/AppSidebar/index.vue
+++ src/Layout/components/AppSidebar/index.vue
@@ -1,61 +1,10 @@
 <script setup lang="ts">
-import { computed } from 'vue'
-import { useRoute, useRouter } from 'vue-router'
-import { routerStore } from '@/store/modules/routerStore'
-import { systemStore } from '@/store/modules/systemStore'
-import AppLink from './components/AppLink.vue';
-import type { IRoute } from './types';
-import PageTitleUtils from '@/utils/PageTitleUtils'
-
-const routeStore = routerStore()
-const route = useRoute()
-const rouetrIntance = useRouter()
-const systemInstance = systemStore()
-const title = PageTitleUtils.getPageTitle('');
-
-const onlyChildRoute: any = computed(() => routeStore.routes.filter(item => !item.hidden && item.children?.length === 1))
-const childRoute: any = computed(() => routeStore.routes.filter(item => !item.hidden && item.children?.length !== 1))
-
-const currentOpenMenu = (): string => {
-  return route.fullPath
-}
-
-function handleFirstMenuRoute(item: IRoute) {
-  rouetrIntance.push({
-    path: item.path
-  })
-}
-
+import SideMenu from './components/SideMenu.vue';
 </script>
 
 <template>
-  <el-menu :collapse="systemInstance.isCollapse" :default-active="currentOpenMenu()" class="el-menu-vertical">
-
-    <el-menu-item v-for="item in onlyChildRoute" :index="item.children[0].path" :key="item.children[0].path">
-      <el-icon @click="handleFirstMenuRoute(item)">
-        <component :is="item.meta?.icon" />
-      </el-icon>
-      <template #title>
-        <app-link :to="item.children[0].path"> {{ item.children[0].title }} </app-link>
-      </template>
-    </el-menu-item>
-
-    <el-sub-menu v-for="item in childRoute" :index="item.path || item.redirect" :key="item.children[0].path">
-      <template #title>
-        <el-icon>
-          <component :is="item.meta?.icon" />
-        </el-icon>
-        <span>{{ item.title }}</span>
-      </template>
-      <el-menu-item v-for="childItem in item.children" :index="childItem.path" :key="childItem.path">
-        <app-link :to="childItem.path"> {{ childItem.title }} </app-link>
-      </el-menu-item>
-    </el-sub-menu>
-  </el-menu>
+  <side-menu></side-menu>
 </template>
 
 <style scoped lang="scss">
-.el-menu-vertical {
-  height: 100%;
-}
 </style>
--- src/Layout/index.vue
+++ src/Layout/index.vue
@@ -13,7 +13,7 @@ const systemInstance = systemStore()
     <div class="side-box" :class="{ 'is-collapse': systemInstance.isCollapse }">
       <app-sidebar />
     </div>
-    <div class="app-content">
+    <div class="app-content" :class="{ 'is-collapse': systemInstance.isCollapse }">
       <app-navbar></app-navbar>
       <app-main></app-main>
     </div>
@@ -24,20 +24,30 @@ const systemInstance = systemStore()
 @import "@/styles/variables.scss";
 
 .main-box {
-  display: flex;
+  position: relative;
   height: calc(100vh - 51px);
 
   .side-box {
+    position: fixed;
+    left: 0;
+    top: 0;
+    height: 100%;
     width: $--menu-width;
     flex-shrink: 0;
 
     &.is-collapse {
-      width: 64px;
+      width: $--collapse-width;
+      transition: width 0.36s;
     }
   }
 
   .app-content {
-    flex: 1;
+    width: 100%;
+    margin-left: $--menu-width;
+    transition: margin-left 0.36s;
+    &.is-collapse {
+      margin-left: $--collapse-width;
+    }
   }
 }
 </style>

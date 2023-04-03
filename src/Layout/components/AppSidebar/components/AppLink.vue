03fa361ba1b520c32ce8bd1d812084b5ae437fd8
liushengkai <949919743@qq.com>
2023-04-03 15:57:34 +0800

    to url

--- src/Layout/components/AppSidebar/components/AppLink.vue
+++ src/Layout/components/AppSidebar/components/AppLink.vue
@@ -1,10 +1,15 @@
 <script setup lang="ts">
 import { useRouter } from 'vue-router'
+import VerifyUtils from '@/utils/VerifyUtils';
 
 const router = useRouter();
 const props = defineProps(['to'])
 
 function handleTo() {
+  if (VerifyUtils.validURL(props.to)) {
+    window.open(props.to, "_blank")
+    return
+  }
   router.push({
     path: props.to
   })

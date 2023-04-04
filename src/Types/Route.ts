4af12a9017c2c55963362cc72ab15650fbac8391
liushengkai <949919743@qq.com>
2023-04-04 18:38:13 +0800

    note

--- src/Types/Route.ts
+++ src/Types/Route.ts
@@ -1,13 +1,13 @@
 export interface IRoute {
-  path: string,
-  redirect: string,
-  meta: IRouteMeta,
   component: any
+  path: string,
+  redirect?: string, /* 重定向地址 */
+  meta?: IRouteMeta,
 }
 
 interface IRouteMeta {
-  icon: String,
-  activeMenu: String,
-  title: string,
-  noBreadCrumbs: Boolean
+  title: string,  /* 对应面包屑和 Menu 菜单 */
+  icon?: String,  /* 图标, 目前只支持 element-plus 的 icon 或者线上图片地址, 可自行修改 */
+  activeMenu?: String,  /* 页面显示时对应的激活 Menu Path */
+  noBreadCrumbs?: Boolean /* 如果为true 则不显示在面包屑 */
 }

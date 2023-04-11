fb265b64f2197aaee9cda6cd0eb6f58f04e30fb8
liushengkai <949919743@qq.com>
2023-04-11 14:33:01 +0800

    router permission describe

--- src/store/modules/routerStore.ts
+++ src/store/modules/routerStore.ts
@@ -13,7 +13,7 @@ export const routerStore = defineStore('routerStore', {
   },
   actions: {
     /**
-     * 请求到路由之后把接口返回的路由当作 Menu菜单 (即访问入口)，但是会默认加载所有的动态路由
+     * 请求到路由之后把接口返回的路由当作 Menu菜单 (即访问入口)，但是会默认添加所有的动态路由
      * 在用户从url上输入无访问权限的路由访问页面时, 建议由接口返回无操作权限code,从而跳转异常页
      */
     async setRoutes() {

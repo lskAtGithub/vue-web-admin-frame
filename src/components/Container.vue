33b9a901e6307936437d6a1b649899e86194440f
liushengkai <949919743@qq.com>
2023-04-14 18:29:04 +0800

    container

--- src/components/Container.vue
+++ src/components/Container.vue
@@ -2,17 +2,17 @@
 </script>
 
 <template>
-  <div class="container">
+  <div class="__common-component-container">
     <slot></slot>
   </div>
 </template>
 
 <style scoped lang='scss'>
-.container {
-  margin: 20px;
+.__common-component-container {
+  margin: 12px;
   background: #fff;
   height: calc(100% - 40px);
-  padding: 20px;
+  padding: 30px 20px 20px 20px;
   border-radius: 5px;
   box-shadow: 0 0 5px #eee;
 }

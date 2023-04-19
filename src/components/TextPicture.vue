df984c816d13348db29c0c511e4780e98818ce46
liushengkai <949919743@qq.com>
2023-04-19 17:10:33 +0800

    text picture

---
+++ src/components/TextPicture.vue
@@ -0,0 +1,34 @@
+<!-- 
+  * 可设置背景图片的文字
+ -->
+
+<script setup lang="ts">
+  const props = defineProps({
+    backgroundImg: {
+      type: String,
+      defualt: ''
+    },
+    fontSize: {
+      type: String,
+      defalut: '14px'
+    }
+  })
+</script>
+
+<template>
+  <div class="text-picture">
+    <slot></slot>
+  </div>
+</template>
+
+<style scoped lang='scss'>
+.text-picture {
+  display: inline-block;
+  background-clip: text;
+  -webkit-background-clip: text;
+  color: transparent;
+  background-size: cover;
+  background-image: v-bind('"url(" + props.backgroundImg + ")"');
+  font-size: v-bind('props.fontSize');
+}
+</style>

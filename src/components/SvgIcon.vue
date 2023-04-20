14f43a36abba9f86386641e06d2df892053cf2da
liushengkai <949919743@qq.com>
2023-04-20 17:04:36 +0800

    svg-icon

---
+++ src/components/SvgIcon.vue
@@ -0,0 +1,37 @@
+<template>
+  <svg :class="svgClass" aria-hidden="true">
+    <use :xlink:href="iconName" />
+  </svg>
+</template>
+<script lang="ts" setup>
+  import { computed } from 'vue'
+  const props = defineProps({
+    iconClass: {
+      type: String,
+      required: true
+    },
+    className: {
+      type: String,
+      default: ''
+    }
+  })
+  const svgClass = computed(() => {
+    if (props.className) {
+      return 'svg-icon ' + props.className
+    } else {
+      return 'svg-icon'
+    }
+  });
+  const iconName = computed(() => {
+    return `#icon-${props.iconClass}`
+  })
+
+</script>
+<style lang="scss" scoped>
+.svg-icon {
+  width: 1em;
+  height: 1em;
+  fill: currentColor;
+  vertical-align: middle;
+}
+</style>
--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { fileURLToPath, URL } from 'node:url'
-
+// @ts-ignore
+import { createSvg } from './src/icons/index'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
@@ -7,10 +8,18 @@ import VueSetupExtend from 'vite-plugin-vue-setup-extend'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueJsx(), VueSetupExtend()],
+  plugins: [
+    vue(),
+    vueJsx(),
+    VueSetupExtend(),
+    createSvg('./src/icons/svg/')
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
+  },
+  optimizeDeps: {
+    include: ['countup.js']
   }
 })

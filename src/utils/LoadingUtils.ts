74e6ebb6decbea746c100a1d97a1fe20cd9fcb32
liushengkai <949919743@qq.com>
2023-06-09 19:15:50 +0800

    request loading only

--- src/utils/LoadingUtils.ts
+++ src/utils/LoadingUtils.ts
@@ -2,20 +2,28 @@ import { ElLoading, type LoadingOptions } from 'element-plus'
 
 
 class LoadingUtils {
+  static loadings: number[] = []
   static loadingInstance: any = null
 
   static show(options?: LoadingOptions) {
-    const defaultOptions: LoadingOptions = {
-      lock: true,
-      text: 'Loading',
-      background: 'rgba(0, 0, 0, 0.7)',
+    if (this.loadings.length) {
+      this.loadings.push(1)
+    } else {
+      const defaultOptions: LoadingOptions = {
+        lock: true,
+        text: 'Loading',
+        background: 'rgba(0, 0, 0, 0.7)',
+      }
+      const LoadingOptions = Object.assign(defaultOptions, options)
+      this.loadingInstance = ElLoading.service(LoadingOptions)
     }
-    const LoadingOptions = Object.assign(defaultOptions, options)
-    this.loadingInstance = ElLoading.service(LoadingOptions)
   }
 
   static hide() {
-    this.loadingInstance.close()
+    this.loadings.pop()
+    if (this.loadings.length === 0) {
+      this.loadingInstance.close()
+    }
   }
 }
 

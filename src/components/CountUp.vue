3b4c5856abda108ed3355b614793ae49200d4d20
liushengkai <949919743@qq.com>
2023-06-13 16:24:27 +0800

    countup

--- src/components/CountUp.vue
+++ src/components/CountUp.vue
@@ -28,13 +28,13 @@
           decimalPlaces: 0, // number类型 小数位，整数自动添.00
           duration: 1, // number类型 动画延迟秒数，默认值是2
           useGrouping: true, // boolean类型  是否开启逗号，默认true(1,000)false(1000)
-          useEasing: true,  // booleanl类型 动画缓动效果(ease),默认true
-          smartEasingThreshold: 300, // numberl类型 大于这个数值的值开启平滑缓动
-          smartEasingAmount: 300, // numberl类型
+          useEasing: true,  // boolean类型 动画缓动效果(ease),默认true
+          smartEasingThreshold: 300, // number类型 大于这个数值的值开启平滑缓动
+          smartEasingAmount: 300, // number类型
           separator: ',',// string 类型 分割用的符号
           decimal: '.', // string 类型  小数分割符合
-          prefix: '', // sttring 类型  数字开头添加固定字符
-          suffix: '', // sttring类型 数字末尾添加固定字符
+          prefix: '', // string 类型  数字开头添加固定字符
+          suffix: '', // string类型 数字末尾添加固定字符
           numerals: []  // Array类型 替换从0到9对应的字，也就是自定数字字符了,数组存储
         }
         return options

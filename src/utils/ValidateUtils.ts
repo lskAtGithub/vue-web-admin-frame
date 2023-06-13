2d4ae623841c3e47d20a35310ea29ccdfe4674c8
liushengkai <949919743@qq.com>
2023-06-13 10:11:10 +0800

    remove validate

--- src/utils/ValidateUtils.ts
+++
@@ -1,9 +0,0 @@
-export default class ValidateUtils {
-  /**
-   * 密码必须包含大小写字母,特殊字符和数字，长度为8-12位
-   */
-  static validatePassword(password: string) {
-    const regexp = /^(?=.*[0-9])(?=.*[A-Z])(?=.*[a-z])(?=.*[!@#$%^&*,\._])[0-9a-zA-Z!@#$%^&*,\\._]{8,12}$/
-    return regexp.test(password)
-  }
-}

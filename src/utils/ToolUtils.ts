6b6faefb42f0fbbd8b3af109b942469e12acc97d
liushengkai <949919743@qq.com>
2023-06-13 14:47:55 +0800

    添加工具方法类

---
+++ src/utils/ToolUtils.ts
@@ -0,0 +1,27 @@
+interface KeyValueObject {
+  [key: string]: any
+}
+
+export default class ToolUtils {
+  /**
+   * @param {object} obj
+   * @returns {object} newObj
+   * @description 对象的深拷贝
+   */
+  static deepClone(obj: KeyValueObject): KeyValueObject {
+    const newObj: KeyValueObject = {};
+    let keys = Object.keys(obj);
+    let key = null;
+    let data = null;
+    for (let i = 0; i < keys.length; i++) {
+      key = keys[i];
+      data = (obj)[key];
+      if (data && typeof data === 'object') {
+        newObj[key] = this.deepClone(data)
+      } else {
+        newObj[key] = data;
+      }
+    }
+    return newObj
+  }
+}

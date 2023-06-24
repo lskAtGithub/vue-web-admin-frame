baa572cd88b27dcd45b7b0988f805c4a9f80d76d
liushengkai <949919743@qq.com>
2023-06-24 23:37:35 +0800

    登录

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,8 +1,9 @@
 <script setup lang="ts">
   import { onMounted } from 'vue'
   import useStore from '@/store'
-
+  import { title } from '@/setting'
   import { useRouter } from 'vue-router'
+  import cat from '@/assets/login/cat.png'
 
   const router = useRouter()
   const { user } = useStore()
@@ -24,26 +25,47 @@
 </script>
 
 <template>
-  <div class="login-model">
-    <p>
-      <el-input v-model="accoutInfo.account" placeholder="请输入账号" />
-    </p>
-    <p>
-      <el-input v-model="accoutInfo.password" type="password" placeholder="请输入密码" show-password />
-    </p>
-    <p>
-      <el-button type="primary" plain @click="handleLogin">登录</el-button>
-    </p>
+  <div class="container">
+    <div class="login-model">
+      <h2>{{ title }} </h2>
+      <p class="desc">开箱即用的后台管理系统框架</p>
+      <p>
+        <el-input v-model="accoutInfo.account" size="large" placeholder="请输入账号" />
+      </p>
+      <p>
+        <el-input v-model="accoutInfo.password" size="large" type="password" placeholder="请输入密码" show-password />
+      </p>
+      <p>
+        <el-button class="login-btn" type="primary" plain @click="handleLogin">登录</el-button>
+        <img :src="cat" alt="cat">
+      </p>
+    </div>
   </div>
 </template>
 
 <style lang="scss" scoped>
+.container {
+  width: 100vw;
+  height: 100vh;
+  padding: 200px 0;
+  background: linear-gradient(to right,#97c6fe, #f2f5ff);
+  .desc {
+    color: #aaa;
+    font-size: 12px;
+  }
+  .login-btn{
+    width: 80%;
+  }
+}
+
 .login-model {
   width: 500px;
   padding: 50px 80px;
   border-radius: 5px;
   background: #f1f1f1;
-  box-shadow: 0 0 5px 0 #eee;
-  margin: 200px auto 0;
+  box-shadow: 0 0 5px 0 #97c6fe;
+  margin: 0 auto;
+  background: url('../../assets/login/login_background.jpg') no-repeat;
+  background-size: cover;
 }
 </style>

95d1d37452140efe9f5762740ebf946005dabe89
liushegnkai <949919743@qq.com>
2023-07-16 20:03:42 +0800

    feat： 控件权限

--- src/directives/modules/permission.ts
+++ src/directives/modules/permission.ts
@@ -1,19 +1,17 @@
 import useStore from '@/store/index'
+import type { Directive, DirectiveBinding } from 'vue'
 
 const { permission } = useStore()
 const { permissions } = permission
 
 function checkPermission(el: Element, binding: { value: any }) {
   const { value } = binding
-
   if (value && value instanceof Array) {
     if (value.length > 0) {
       const permissionRoles = value
-
       const hasPermission = permissions.some(role => {
         return permissionRoles.includes(role)
       })
-
       if (!hasPermission) {
         el.parentNode && el.parentNode.removeChild(el)
       }
@@ -23,11 +21,13 @@ function checkPermission(el: Element, binding: { value: any }) {
   }
 }
 
-export default {
-  created(el: Element, binding: any) {
+const instance: Directive = {
+  created(el: HTMLElement, binding: DirectiveBinding) {
     checkPermission(el, binding)
   },
-  updated(el: Element, binding: any) {
+  updated(el: HTMLElement, binding: DirectiveBinding) {
     checkPermission(el, binding)
   }
 }
+
+export default { name: 'permission', instance }
--- src/directives/permission/index.ts
+++
@@ -1,8 +0,0 @@
-import type { App } from 'vue'
-import permission from './permission'
-
-const install = function(app:App) {
-  app.directive('permission', permission)
-}
-
-export default install

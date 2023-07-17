659e510536af729ff75932ec5c19c218b0698efc
liushegnkai <949919743@qq.com>
2023-07-17 21:29:02 +0800

    feat：Top bar Message modules

--- src/Layout/components/AppNavbar/components/FunctionalArea.vue
+++ src/Layout/components/AppNavbar/components/FunctionalArea.vue
@@ -6,7 +6,7 @@
     <svg-icon class-name="svg-icon" icon-class="fullscreen"></svg-icon>
     <svg-icon class-name="svg-icon" icon-class="fullscreen-exit"></svg-icon>
 
-    <el-popover placement="bottom" :width="200" trigger="hover">
+    <el-popover placement="bottom" :width="300" trigger="hover">
       <template #reference>
         <el-badge :is-dot="true">
           <el-icon class="icon-box">
@@ -15,19 +15,41 @@
         </el-badge>
       </template>
       <div class="message-box">
-        this is message
+        <el-tabs v-model="activeName" class="demo-tabs" stretch>
+          <el-tab-pane label="未读" name="wait">
+            <el-collapse accordion>
+              <el-collapse-item v-for="(item, index) in messageList" :key="index" :title="item.title" :name="index">
+                <div class="message-content">
+                  {{ item.content }}
+                </div>
+              </el-collapse-item>
+            </el-collapse>
+          </el-tab-pane>
+          <el-tab-pane label="已读" name="done">
+            <el-empty description="暂无已读消息" />
+          </el-tab-pane>
+        </el-tabs>
       </div>
     </el-popover>
   </div>
 </template>
 
 <script lang="ts" setup name="FunctionalArea">
-  import { ref, reactive } from 'vue'
+  import { ref } from 'vue'
 
+  let activeName = ref('wait')
+  const messageList = ref([
+    { title: 'Message Title', content: 'This is a notification message. Please reply upon receipt' },
+    { title: 'Message Title', content: 'This is a notification message. Please reply upon receipt' },
+    { title: 'Message Title', content: 'This is a notification message. Please reply upon receipt' },
+    { title: 'Message Title', content: 'This is a notification message. Please reply upon receipt' },
+    { title: 'Message Title', content: 'This is a notification message. Please reply upon receipt' },
+  ])
 </script>
 
 <style scoped lang="scss">
 @import '@/styles/variables.scss';
+@import '@/styles/mixin.scss';
 
 .container {
   font-size: 20px;

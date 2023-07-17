5d2647a6d534247bed69dfde000616f211d609e5
liushegnkai <949919743@qq.com>
2023-07-17 21:49:53 +0800

    feat：topbar 功能菜单

---
+++ src/Layout/components/AppNavbar/components/FunctionalArea/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="container">
+    <search-menu />
+    <fullscreen />
+    <message />
+  </div>
+</template>
+
+<script lang="ts" setup name="FunctionalArea">
+  import Fullscreen from './Fullscreen.vue'
+  import Message from './Message.vue'
+  import SearchMenu from './SearchMenu.vue'
+</script>
+
+<style scoped lang="scss">
+.container {
+  font-size: 20px;
+  display: flex;
+  align-items: center;
+}
+</style>

23ffcfea59c9dc0a89988e122eb5c3cbc2cc50ea
liushengkai <949919743@qq.com>
2023-08-08 11:38:01 +0800

    feat：tinymce

--- src/components/Tinymce/toolbar.ts
+++ src/components/Tinymce/toolbar.ts
@@ -1,6 +1,6 @@
 // emoticons 表情
 const toolbar: string[] = [
-  'indent2em undo redo bold styleselect formatselect fontselect fontsizeselect italic underline strikethrough letterspacing lineheight alignleft aligncenter alignright outdent indent searchreplace  blockquote cut copy paste pastetext alignjustify removeformat subscript superscript code codesample hr bullist numlist link image charmap preview anchor pagebreak insertdatetime media table forecolor backcolor importword fullscreen print'
+  'indent2em undo redo bold styleselect formatselect fontselect fontsizeselect italic underline strikethrough letterspacing lineheight alignleft aligncenter alignright outdent indent searchreplace  blockquote alignjustify removeformat subscript superscript code codesample hr bullist numlist link image charmap preview anchor pagebreak insertdatetime media table forecolor backcolor importword fullscreen upfile print'
 ]
 
 export default toolbar
